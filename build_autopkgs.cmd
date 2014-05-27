bda57347a8d35a31de55a4f595f92d987da983ee
Kevin Smyth <kevin.m.smyth@gmail.com>
2014-05-27 09:58:42 -0500

    Version 3.7.3

--- build_autopkgs.cmd
+++ build_autopkgs.cmd
@@ -1,8 +1,8 @@
-rem install Gecode x86
-rem xcopy /S  "c:\Program Files (x86)\Gecode" Gecode_x86
-rem uninstall Gecode x86
-rem install Gecode x64
-rem xcopy /S  "c:\Program Files\Gecode" Gecode_x64
-rem uninstall Gecode x64
+start /wait msiexec /i %userprofile%\downloads\Gecode-3.7.3-x86-2010.msi /qn ALLUSERS=1
+xcopy /S/I  "c:\Program Files (x86)\Gecode" Gecode_x86
+start /wait msiexec /x %userprofile%\downloads\Gecode-3.7.3-x86-2010.msi /qn
+start /wait msiexec /i %userprofile%\downloads\Gecode-3.7.3-x64-2010.msi /qn ALLUSERS=1
+xcopy /S/I  "c:\Program Files\Gecode" Gecode_x64
+start /wait msiexec /x %userprofile%\downloads\Gecode-3.7.3-x64-2010.msi /qn
 powershell -ExecutionPolicy ByPass ipmo \"C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.Powershell.Tools.dll\" ; Write-NuGetPackage .\gecode.autopkg
 powershell -ExecutionPolicy ByPass ipmo \"C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.Powershell.Tools.dll\" ; Write-NuGetPackage .\gecode-vc100.autopkg
