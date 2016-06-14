0ed5b1a034381b8fff374f9fff986866ae2da042
Kevin Smyth <kevin.m.smyth@gmail.com>
2016-06-14 09:39:34 -0500

    Gecode 4.4.0 vc140

--- build_autopkgs.cmd
+++ build_autopkgs.cmd
@@ -1,8 +1,13 @@
-rem install Gecode x86
-rem xcopy /S  "c:\Program Files (x86)\Gecode" Gecode_x86
-rem uninstall Gecode x86
-rem install Gecode x64
-rem xcopy /S  "c:\Program Files\Gecode" Gecode_x64
-rem uninstall Gecode x64
-powershell -ExecutionPolicy ByPass ipmo \"C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.Powershell.Tools.dll\" ; Write-NuGetPackage .\gecode.autopkg
-powershell -ExecutionPolicy ByPass ipmo \"C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.Powershell.Tools.dll\" ; Write-NuGetPackage .\gecode-vc100.autopkg
+Setlocal EnableDelayedExpansion
+
+msiexec.exe /i Gecode-4.4.0-x86-2015.msi /qn || exit /b !ERRORLEVEL!
+xcopy /S/D  "c:\Program Files (x86)\Gecode" Gecode_x86\
+msiexec.exe /x Gecode-4.4.0-x86-2015.msi /qn || exit /b !ERRORLEVEL!
+
+msiexec.exe /i Gecode-4.4.0-x64-2015.msi /qn || exit /b !ERRORLEVEL!
+xcopy /S/D  "c:\Program Files\Gecode" Gecode_x64\
+msiexec.exe /x Gecode-4.4.0-x64-2015.msi /qn || exit /b !ERRORLEVEL!
+
+powershell -ExecutionPolicy ByPass ipmo \"C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.Powershell.Tools.dll\" ; Write-NuGetPackage .\gecode.autopkg || exit /b !ERRORLEVEL!
+rem powershell -ExecutionPolicy ByPass ipmo \"C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.Powershell.Tools.dll\" ; Write-NuGetPackage .\gecode-vc100.autopkg || exit /b !ERRORLEVEL!
+powershell -ExecutionPolicy ByPass ipmo \"C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.Powershell.Tools.dll\" ; Write-NuGetPackage .\gecode-vc140.autopkg || exit /b !ERRORLEVEL!
